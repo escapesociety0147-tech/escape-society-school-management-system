88c6d4ba8703603af69122a11cf5fd66ded93db3
TomRiddle67 <courage0047@gmail.com>
2026-07-12 06:11:03 -0300

    feat(models): add UserPassword model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,5 +6,6 @@ autogenerates migrations. Each new model file must be imported below.
 
 from app.models.school import School
 from app.models.user import User, UserRole
+from app.models.user_password import UserPassword
 
-__all__ = ["School", "User", "UserRole"]
+__all__ = ["School", "User", "UserRole", "UserPassword"]
